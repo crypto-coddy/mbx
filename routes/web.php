e7765a532719727eb4836e3bb69b079d3d16b31b
Akash Singh <akash.s@aaftonline.com>
2026-06-22 23:31:13 +0530

    v2 trade added

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/admin/login')->name('admin');
+Route::get('/', function () {
+    return redirect('/admin/login');
+})->name('home');
